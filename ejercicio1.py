f4f2de52303b6a57704eefd301317fbde8cc1b70
dalila1-10 <ug0740032026@unab.edu.sv>
2026-04-07 23:05:11 -0600

    Crear una función
    Crear una función que reciba un texto y un número.

---
+++ ejercicio1.py
@@ -0,0 +1,7 @@
+def transformar_texto(texto, numero):
+    if numero == 1:
+        return texto.upper()    
+    elif numero == 2:
+        return texto.lower()    
+    elif numero == 3:
+        return texto.capitalize() 
