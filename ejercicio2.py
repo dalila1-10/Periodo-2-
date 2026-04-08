cdfe3303951d034710f93e5f902a58e0cff83315
dalila1-10 <ug0740032026@unab.edu.sv>
2026-04-07 23:07:35 -0600

    Crear función que reciba una palabra y un número
    Crear una función que reciba una palabra y un número y mostrar el resultado en pantalla

---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+def mostrar_transformacion(palabra, numero):
+    if numero == 1:
+        print(palabra.upper())
+    elif numero == 2:
+        print(palabra.lower())
+    elif numero == 3:
+        print(palabra.capitalize())
